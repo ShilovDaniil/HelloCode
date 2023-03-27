cfa1a7c4dbe320be0407b719cc40385438c3a9fa
DaniilShilov <shilovdanv@gmail.com>
2023-03-27 23:21:56 +0400

    add Example 005 Condition If-Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "daniil")
+{
+  Console.WriteLine("Yeah, it`s Daniil");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
